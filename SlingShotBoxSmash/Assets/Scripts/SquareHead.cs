73656553d35293452ec34b77a8f1e1d858a6701d
Kevin <ksandy@outlook.ie>
2020-08-14 13:08:37 +0100

    Add boss transition animations

--- SlingShotBoxSmash/Assets/Scripts/SquareHead.cs
+++ SlingShotBoxSmash/Assets/Scripts/SquareHead.cs
@@ -9,7 +9,51 @@ public class SquareHead : MonoBehaviour
     public GameObject hitEffect;
     public AudioSource hitSound;
     public Animator anim;
-   
+    Renderer[] renderers;
+
+    private void Start()
+    {
+        gameObject.GetComponent<SpriteRenderer>().color = new Color32(255,0,0,255);
+        renderers = GetComponentsInChildren<Renderer>();
+
+        CameraShake.Instance.ShakeCamera(10f, 5f);
+
+        foreach (Renderer renderer in renderers)
+        {
+            if (renderer.gameObject.tag.Equals("HitPoint"))
+            {
+                continue;
+            }
+            renderer.material.color = new Color32(255, 0, 0, 255);
+        }
+    }
+
+    private void Flash()
+    {
+        foreach (Renderer renderer in renderers)
+        {
+            if (renderer.gameObject.tag.Equals("HitPoint"))
+            {
+                continue;
+            }
+            renderer.material.color = new Color32(0, 245, 255, 255);
+        }
+        
+        Invoke("ResetColor", 0.1f);
+    }
+
+    private void ResetColor()
+    {
+        foreach (Renderer renderer in renderers)
+        {
+            if (renderer.gameObject.tag.Equals("HitPoint"))
+            {
+                continue;
+            }
+            renderer.material.color = new Color32(255, 0, 0, 255);
+        }
+        
+    }
 
     private void Update()
     {
@@ -24,7 +68,7 @@ public class SquareHead : MonoBehaviour
 
         if (collision.gameObject.tag.Equals("Player") && SlingShot.isHeldDown == false)
         {
-           
+            Flash();
             anim.speed += 0.18f;
             CameraShake.Instance.ShakeCamera(13f, 0.2f);
             Vector3 vel = collision.gameObject.GetComponentInParent<Rigidbody2D>().velocity;
@@ -41,11 +85,6 @@ public class SquareHead : MonoBehaviour
         }
 
         
-    }
-
-    private void StartAnimationAgain()
-    {
-        anim.StartPlayback();
     }
 
     public void Die()
